5656d0fd03719008e0aa03c3b6278a759cfbb743
KARTHIKEAYAN2022 <karthikeyan@maventic.com>
2025-02-12 18:07:43 +0530

    changes

--- api.py
+++ api.py
@@ -8,31 +8,12 @@ app = FastAPI()
 # Load the CSV data
 def load_data():
     df = pd.read_csv("Doubtful debts.csv")
-    return df
+    return df.to_dict(orient="records")
 
-df = load_data()
+data = load_data()
 
 @app.get("/doubtful_debts")
 async def doubtful_debts():
-    odata_json = {
-        "d": {
-            "results": []
-        }
-    }
+    return {"@odata.context": "https://emamai-poc.onrender.com/doubtful_debts/$metadata#doubtful_debts",
+        "value":data}
 
-    for _, row in df.iterrows():
-        entry = {
-            "__metadata": {
-                "id": f"Customer({row['Customer']})",
-                "uri": f"/odata/service/Customer({row['Customer']})",
-                "type": "Namespace.Customer"
-            },
-            "FiscalYear": row["Fiscal Year"],
-            "Customer": row["Customer"],
-            "CompanyCode": row["Company code"],
-            "DoubtfulDebts": row["Doubtful debts"],
-            "Unit": row["Unit"]
-        }
-        odata_json["d"]["results"].append(entry)
-    
-    return odata_json
