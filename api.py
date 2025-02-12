c9df2a95e311838588253837dbbcdb089b40bff0
KARTHIKEAYAN2022 <karthikeyan@maventic.com>
2025-02-12 18:54:54 +0530

    xml

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from typing import List
 from pydantic import BaseModel
 import pandas as pd
@@ -17,3 +17,31 @@ async def doubtful_debts():
     return {"@odata.context": "https://emamai-poc.onrender.com/$metadata#doubtful_debts",
         "value":data}
 
+@app.get("/doubtful_debts/$metadata")
+async def get_metadata():
+    metadata = """
+    <?xml version="1.0" encoding="utf-8" ?>
+    <edmx:Edmx Version="4.0" xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx">
+        <edmx:DataServices>
+            <Schema Namespace="YourNamespace" xmlns="http://docs.oasis-open.org/odata/ns/edm">
+                <EntityType Name="DoubtfulDebt">
+                    <Key>
+                        <PropertyRef Name="FiscalYear" />
+                        <PropertyRef Name="Customer" />
+                    </Key>
+                    <Property Name="FiscalYear" Type="Edm.String" Nullable="false" />
+                    <Property Name="Customer" Type="Edm.String" Nullable="false" />
+                    <Property Name="CompanyCode" Type="Edm.String" Nullable="false" />
+                    <Property Name="DoubtfulDebts" Type="Edm.Decimal" Nullable="false" />
+                    <Property Name="Unit" Type="Edm.String" Nullable="false" />
+                </EntityType>
+                <EntityContainer Name="Container">
+                    <EntitySet Name="doubtful_debts" EntityType="YourNamespace.DoubtfulDebt" />
+                </EntityContainer>
+            </Schema>
+        </edmx:DataServices>
+    </edmx:Edmx>
+    """
+    return Response(content=metadata, media_type="application/xml")
+
+# Add additional routes and middleware as needed
